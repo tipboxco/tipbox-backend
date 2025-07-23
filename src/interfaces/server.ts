3237847af2dfc85163bf92311aabb12430a4817e
Ömer Faruk DEMİRAL <omer.frk027@gmail.com>
2025-07-23 17:33:59 +0300

    package.json ve package-lock.json dosyalarında @prisma/client bağımlılığı 6.12.0 sürümüne güncellendi; dotenv-cli ve @types/uuid bağımlılıkları eklendi. server.ts dosyasına dotenv yapılandırması ve JWT_SECRET log'u eklendi.

--- src/interfaces/server.ts
+++ src/interfaces/server.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import app from './app';
 import logger from '../infrastructure/logger/logger';
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
