3a1e348d9d23736f06025e72cb9a48a1674be4da
iguzelofficial <iguzel657@gmail.com>
2026-01-27 17:46:04 +0300

    feat: Update environment configuration and package dependencies
    - Added ADMIN_PRIVATE_KEY and THIRDWEB_SECRET_KEY to example environment files for production, test, and development setups.
    - Updated package dependencies to include the thirdweb library version 5.118.0, enhancing blockchain integration capabilities.
    - Removed obsolete products_with_images.csv file to streamline data management.

---
+++ backend/src/infrastructure/config/web3-config/abi.config.ts
@@ -0,0 +1,255 @@
+// Tipbox Contract ABI
+export const TIPBOX_ABI = [
+  {
+    inputs: [{ name: "", type: "address" }],
+    name: "pendingTips",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "feePercentage",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "feeRecipient",
+    outputs: [{ name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { name: "amount", type: "uint256" },
+      { name: "target", type: "address" },
+    ],
+    name: "tip",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "claim",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
+
+// ERC20 Token ABI
+export const ERC20_ABI = [
+  {
+    inputs: [{ name: "account", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { name: "spender", type: "address" },
+      { name: "amount", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { name: "owner", type: "address" },
+      { name: "spender", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
+
+// TiboxBadges ERC721 ABI (Custom contract with safeMint)
+export const ERC721_ABI = [
+  {
+    inputs: [{ name: "owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ name: "tokenId", type: "uint256" }],
+    name: "ownerOf",
+    outputs: [{ name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ name: "tokenId", type: "uint256" }],
+    name: "tokenURI",
+    outputs: [{ name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  // safeMint function - only owner can call
+  {
+    inputs: [
+      { name: "to", type: "address" },
+      { name: "uri", type: "string" },
+    ],
+    name: "safeMint",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [{ name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [{ name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { name: "owner", type: "address" },
+      { name: "index", type: "uint256" },
+    ],
+    name: "tokenOfOwnerByIndex",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ name: "index", type: "uint256" }],
+    name: "tokenByIndex",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
+
+// NFT Drop / Claimable ABI (thirdweb NFT Drop contract)
+export const NFT_DROP_ABI = [
+  {
+    inputs: [
+      { name: "receiver", type: "address" },
+      { name: "quantity", type: "uint256" },
+    ],
+    name: "claim",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { name: "receiver", type: "address" },
+      { name: "quantity", type: "uint256" },
+      { name: "currency", type: "address" },
+      { name: "pricePerToken", type: "uint256" },
+      { name: "allowlistProof", type: "tuple",
+        components: [
+          { name: "proof", type: "bytes32[]" },
+          { name: "quantityLimitPerWallet", type: "uint256" },
+          { name: "pricePerToken", type: "uint256" },
+          { name: "currency", type: "address" },
+        ]
+      },
+      { name: "data", type: "bytes" },
+    ],
+    name: "claim",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "claimCondition",
+    outputs: [
+      { name: "currentStartId", type: "uint256" },
+      { name: "count", type: "uint256" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ name: "conditionId", type: "uint256" }],
+    name: "getClaimConditionById",
+    outputs: [
+      {
+        name: "condition",
+        type: "tuple",
+        components: [
+          { name: "startTimestamp", type: "uint128" },
+          { name: "maxClaimableSupply", type: "uint128" },
+          { name: "supplyClaimed", type: "uint128" },
+          { name: "quantityLimitPerWallet", type: "uint128" },
+          { name: "merkleRoot", type: "bytes32" },
+          { name: "pricePerToken", type: "uint256" },
+          { name: "currency", type: "address" },
+          { name: "metadata", type: "string" },
+        ],
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "nextTokenIdToMint",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ name: "owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ name: "tokenId", type: "uint256" }],
+    name: "tokenURI",
+    outputs: [{ name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
---
+++ backend/src/infrastructure/config/web3-config/index.ts
@@ -0,0 +1,3 @@
+export { CHAIN_CONFIG, sepoliaChain } from "./chain.config";
+export { CONTRACT_ADDRESSES, type ContractName } from "./contracts.config";
+export { TIPBOX_ABI, ERC20_ABI, ERC721_ABI, NFT_DROP_ABI } from "./abi.config";
