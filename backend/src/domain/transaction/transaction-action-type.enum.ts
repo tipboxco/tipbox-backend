679186df13593671922baef69a39ba00d1ee391b
iguzelofficial <iguzel657@gmail.com>
2026-02-27 14:34:23 +0300

    chore: Update Docker Compose files for production and testing environments
    - Standardized the usage of environment variables for database configurations in docker-compose.prod.yml and docker-compose.test.yml.
    - Updated comments for clarity regarding the use of the .env file.
    - Enhanced healthcheck commands to utilize environment variables for user credentials.
    - Improved consistency in environment variable definitions across services.

--- backend/src/domain/transaction/transaction-action-type.enum.ts
+++ backend/src/domain/transaction/transaction-action-type.enum.ts
@@ -11,7 +11,7 @@ export enum TransactionActionType {
   FEE = 'FEE',
   // External wallet işlemleri
   DEPOSIT = 'DEPOSIT',       // External wallet'tan (Metamask vb.) gelen token
-  WITHDRAW = 'WITHDRAW'      // External wallet'a gönderilen token
-  BOOST_POST = 'BOOST_POST'
+  WITHDRAW = 'WITHDRAW',     // External wallet'a gönderilen token
+  BOOST_POST = 'BOOST_POST',
 }
 
