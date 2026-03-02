3f965468ada7d8f517f8f9b7e0500dfb5ad56528
Ömer Faruk DEMİRAL <omer.frk027@gmail.com>
2026-03-02 16:44:29 +0300

    fix: Add allowedHosts configuration for the development server

--- admin-panel/vite.config.ts
+++ admin-panel/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5174,
+    allowedHosts: ['api-test.tipbox.co'],
     proxy: {
       '/admin': {
         target: 'http://localhost:3000',
