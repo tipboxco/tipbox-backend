a3354a3729b8b921f68b310af972cb1f5614123f
iguzelofficial <iguzel657@gmail.com>
2026-03-04 17:51:40 +0300

    some update

--- catalog-service/medusa-config.ts
+++ catalog-service/medusa-config.ts
@@ -2,6 +2,9 @@ import { loadEnv, defineConfig, Modules } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
+  server:{
+    hmr:false
+  },
   admin: {
     path: process.env.MEDUSA_ADMIN_PATH || "/app",
     backendUrl: process.env.MEDUSA_BACKEND_URL || undefined,
